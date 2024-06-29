9be884c8c244560c0361e22d497cb768cde377a8
Joe Trabulsy <jtrabulsy@gmail.com>
2024-06-29 19:04:29 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyvesync',
-    version='2.1.11',
+    version='2.1.12',
     description='pyvesync is a library to manage Etekcity\
                  Devices, Cosori Air Fryers and Levoit Air \
                      Purifiers run on the VeSync app.',
