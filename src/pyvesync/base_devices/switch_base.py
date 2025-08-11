6182084de3065c12d4b48d79c30a2c3e1ed4b5a7
SapuSeven <sapuseven@gmail.com>
2025-08-12 00:34:26 +0200

    Update package name in documentation

--- src/pyvesync/base_devices/switch_base.py
+++ src/pyvesync/base_devices/switch_base.py
@@ -102,7 +102,7 @@ class VeSyncSwitch(VeSyncBaseToggleDevice):
     """Etekcity Switch Base Class.
 
     Abstract Base Class for Etekcity Switch Devices, inheriting from
-    pyvesync.vesyncbasedevice.VeSyncBaseDevice. Should not be instantiated directly,
+    pyvesync.base_devices.vesyncbasedevice.VeSyncBaseDevice. Should not be instantiated directly,
     subclassed by VeSyncWallSwitch and VeSyncDimmerSwitch.
 
     Attributes:
