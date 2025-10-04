f2942f3fea5a2c61cb495ce3a48c9a0b0fd70874
Joe Trabulsy <jtrabulsy@gmail.com>
2025-10-04 12:49:30 -0500

    Deployed e7626af to 3.0.0rc2 with MkDocs 1.6.1 and mike 2.1.3

--- 3.0.0rc2/hooks.py
+++ 3.0.0rc2/hooks.py
@@ -1,23 +1,28 @@
-
-
+"""Utility hooks for mkDocs."""
 import re
 import urllib.request
 
 
 def replacer(match: re.Match) -> str:
-    """Find and replace github links with code snippets."""
+    """Find and replace github links with code snippets.
+
+    This function parses a Github link and retreives the specified
+    lines of code from the file, formatting it as a markdown code block.
+    """
     filename = f'{match.group(3)}.{match.group(4)}'
-    url = f'https://raw.githubusercontent.com/{match.group(1)}/{match.group(2)}/{filename}'
-    code = urllib.request.urlopen(url).read().decode('utf-8')
+    url = (
+        f'https://raw.githubusercontent.com/{match.group(1)}/{match.group(2)}/{filename}'
+    )
+    code = urllib.request.urlopen(url).read().decode('utf-8')  # noqa: S310
     extension = 'js' if match.group(4) == 'vue' else match.group(4)
     return '\n'.join(
-        [f'``` {extension} title="{filename}"'] +
-        code.split('\n')[int(match.group(5)) - 1:int(match.group(6))] +
-        ['```', f'View at [GitHub]({match.group(0)})']
+        [f'``` {extension} title="{filename}"']
+        + code.split('\n')[int(match.group(5)) - 1: int(match.group(6))]
+        + ['```', f'View at [GitHub]({match.group(0)})']
     )
 
 
-def on_page_markdown(markdown: str, **kwargs) -> str:
+def on_page_markdown(markdown: str, **kwargs) -> str:  # noqa: ARG001, ANN003
     """Replace GitHub links with code snippets."""
     return re.sub(
         re.compile(
