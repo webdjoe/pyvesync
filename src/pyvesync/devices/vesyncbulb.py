4368d506c3878013b1d0a2abb18c6d1ce8fb7f5a
cdnninja <jaydenaphillips@gmail.com>
2026-05-23 21:12:45 -0600

    Update vesyncbulb.py

--- src/pyvesync/devices/vesyncbulb.py
+++ src/pyvesync/devices/vesyncbulb.py
@@ -289,7 +289,7 @@ class VeSyncBulbESL100(BypassV1Mixin, VeSyncBulb):
         }
 
         r_dict = await self.call_bypassv1_api(
-            bulb_models.RequestESL100Detail, method_dict, 'deviceDetail', 'deviceDetail'
+            request_model = bulb_models.RequestESL100Detail, update_dict = method_dict, method = 'deviceDetail', endpoint = 'deviceDetail'
         )
         model = process_bypassv1_result(
             self, logger, 'get_details', r_dict, bulb_models.ResponseESL100DetailResult
@@ -300,7 +300,9 @@ class VeSyncBulbESL100(BypassV1Mixin, VeSyncBulb):
             return
         self.state.brightness = model.brightness
         self.state.device_status = DeviceStatus(model.deviceStatus)
-        self.state.connection_status = ConnectionStatus(model.connectionStatus)
+        #self.state.connection_status = ConnectionStatus(model.connectionStatus)
+        self.state.device_status = DeviceStatus.ON
+        self.state.connection_status = ConnectionStatus.ONLINE
 
     @deprecated(
         'toggle() is deprecated, use toggle_switch(toggle: bool | None = None) instead'
